944b824511ed5b86e200f7abf5930d83b7778788
andreroxhage <an8603ro-s@student.lu.se>
2023-09-14 09:07:57 +0200

    reducer example

--- exercises/reducer_example/src/App.js
+++ exercises/reducer_example/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useReducer} from 'react';
+
+// Define action types for the reducer
+const ACTIONS = {
+	INCREMENT: 'increment',
+	DECREMENT: 'decrement',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Use the useReducer hook to manage state and dispatch actions
+	const [state, dispatch] = useReducer(reducer, {count: 0});
+
+	// Define a reducer function to handle state updates
+	function reducer(state, action) {
+		switch (action.type) {
+			case ACTIONS.INCREMENT:
+				return {count: state.count + 1};
+			case ACTIONS.DECREMENT:
+				return {count: state.count - 1};
+			default:
+				return state;
+		}
+	}
+
+	// Render the component with buttons for incrementing and decrementing
+	return (
+		<>
+			<button onClick={() => dispatch({type: ACTIONS.DECREMENT})}>-</button>
+			<span>{state.count}</span>
+			<button onClick={() => dispatch({type: ACTIONS.INCREMENT})}>+</button>
+		</>
+	);
 }
 
 export default App;

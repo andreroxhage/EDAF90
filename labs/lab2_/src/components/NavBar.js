7e8b05e84f6cab556beb247ce46e54ce93c2a84b
Your Name <lisa.balke@yahoo.se>
2023-09-20 15:12:22 +0200

    Fin navbar, ingen funktionalitet

--- labs/lab2_/src/components/NavBar.js
+++ labs/lab2_/src/components/NavBar.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Link from 'react-router-dom';
 
 export default function NavBar() {
-	return <div></div>;
+	return <nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		<div class="navbar-nav">
+		  <a class="nav-link" href="#">Compose Salad</a>
+		  <a class="nav-link" href="#">Varukorg</a>
+		</div>
+	  </div>
+	</div>
+  </nav>;
+
 }
